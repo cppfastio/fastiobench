876ec3d9c1e42989d7f27665b3cf82b350d4859a
trcrsired <uwgghhbcad@gmail.com>
2024-03-28 17:01:16 -0400

    use .test instead .txt because cmake uses it

--- src/0001.10million/iobuf_file.cc
+++ src/0001.10million/iobuf_file.cc
@@ -9,7 +9,7 @@ int main()
 	constexpr std::size_t N(10000000);
 	{
 		fast_io::timer t(u8"output");
-		fast_io::obuf_file obf("iobuf_file.txt");
+		fast_io::obuf_file obf("iobuf_file.test");
 		for (std::size_t i{}; i != N; ++i)
 		{
 			println(obf, i);
@@ -18,7 +18,7 @@ int main()
 	std::vector<std::size_t> vec(N);
 	{
 		fast_io::timer t(u8"input");
-		fast_io::ibuf_file ibf("iobuf_file.txt");
+		fast_io::ibuf_file ibf("iobuf_file.test");
 		for (std::size_t i{}; i != N; ++i)
 		{
 			scan(ibf, vec[i]);
