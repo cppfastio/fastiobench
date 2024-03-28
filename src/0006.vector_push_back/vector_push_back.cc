8711dc0bff8ce594a57cbb5a4bd4f08c69a82ede
trcrsired <uwgghhbcad@gmail.com>
2024-03-28 17:31:13 -0400

    vector push_back

--- src/0006.vector_push_back/vector_push_back.cc
+++ src/0006.vector_push_back/vector_push_back.cc
@@ -1,5 +1,5 @@
 #include<fast_io.h>
-#include<fast_io_dsal.h>
+#include<fast_io_dsal/vector.h>
 #include<fast_io_driver/timer.h>
 
 int main()
@@ -7,10 +7,10 @@ int main()
 	constexpr ::std::size_t N{20000000};
 	::std::size_t capacity{};
 	{
-		::fast_io::timer timer("::fast_io::vector<::std::size_t>");
+		::fast_io::timer timer_total(u8"::fast_io::vector<::std::size_t>");
 		::fast_io::vector<::std::size_t> vec;
 		{
-			::fast_io::timer timer("::fast_io::vector<::std::size_t> push_back");
+			::fast_io::timer timer_push_back(u8"::fast_io::vector<::std::size_t> push_back");
 			for(::std::size_t i{};i!=N;++i)
 			{
 				vec.push_back(i);
