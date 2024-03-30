ba0f0cbb471e4b7666077751472b221ce2a627fc
trcrsired <uwgghhbcad@gmail.com>
2024-03-30 04:52:15 -0400

    add sha512

---
+++ src/0005.sha/sha512.cc
@@ -0,0 +1,18 @@
+﻿#include <fast_io_dsal/vector.h>
+#include <fast_io.h>
+#include <fast_io_device.h>
+#include <fast_io_crypto.h>
+
+int main(int argc, char **argv)
+{
+	using namespace fast_io::mnp;
+	::fast_io::vector<::std::size_t> vec(10000000,5);
+	{
+		auto t0{fast_io::posix_clock_gettime(fast_io::posix_clock_id::realtime)};
+		fast_io::sha512_context ctx;
+		::fast_io::operations::write_all_range(as_file(ctx),vec);
+		ctx.do_final();
+		println(hash_digest(ctx), " *", os_c_str(argv[1]), "\nElapsed Time:", fast_io::posix_clock_gettime(fast_io::posix_clock_id::realtime) - t0);
+	}
+}
+
