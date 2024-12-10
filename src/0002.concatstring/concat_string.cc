a5ae5767024a0045cfa42a757de688d6431ad0cc
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-12-10 03:06:37 -0500

    Update concat_string.cc

--- src/0002.concatstring/concat_string.cc
+++ src/0002.concatstring/concat_string.cc
@@ -1,4 +1,4 @@
-﻿#include <string>
+#include <fast_io_dsal/string.h>
 #include <fast_io.h>
 #include <version>
 
@@ -28,13 +28,13 @@ template <typename Func> inline benchmark_return benchmark(Func meth) {
 
 inline auto color_concat(std::uint_least8_t r, std::uint_least8_t g,
                          std::uint_least8_t b) {
-  return ::fast_io::concat_std("Red: ", r, ", Green: ", g, ", Blue: ", b);
+  return ::fast_io::concat_fast_io("Red: ", r, ", Green: ", g, ", Blue: ", b);//avoid using concat_std but using fast_io's string instead
 }
 
 int main() {
   auto concat_time = benchmark(color_concat);
   using namespace fast_io::io;
   perrln("concat_string:", concat_time.timestamp, "s");
-  print("fast_io::concat_std (total size: ", concat_time.total_size, ") took ",
+  print("fast_io::concat_fast_io (total size: ", concat_time.total_size, ") took ",
         concat_time.timestamp, "s.\n");
 }
